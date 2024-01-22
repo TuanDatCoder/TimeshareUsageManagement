56d6f9fb2b4afb7522b2646e71ee5dbffeca260d
TuanDatCoder <tuandatdq03@gmail.com>
2024-01-22 10:41:45 +0700

    First commit

---
+++ src/main/java/com/FTimeshare/UsageManagement/TimeshareUsageManagementApplication.java
@@ -0,0 +1,13 @@
+package com.FTimeshare.UsageManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeshareUsageManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeshareUsageManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FTimeshare/UsageManagement/TimeshareUsageManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FTimeshare.UsageManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeshareUsageManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
