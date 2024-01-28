aa6bc1662cff005a24dbbd821a8209d4f12348de
thaovy305 <ngothaovy305@gmail.com>
2024-01-28 13:43:19 +0700

    .

---
+++ src/main/java/dtos/ProjectDto.java
@@ -0,0 +1,18 @@
+package dtos;
+
+import lombok.Data;
+
+@Data
+public class ProjectDto {
+    private String productID;
+    private String productName;
+    private String productDescription;
+    private String productConvenience;
+    private String productArea;
+    private double productPrice;
+    private String productStatus;
+    private String productPicture;
+    private String projectID;
+    private String productTypeID;
+
+}
---
+++ src/main/java/dtos/ProjectTypeDto.java
@@ -0,0 +1,10 @@
+package dtos;
+
+import lombok.Data;
+
+@Data
+
+public class ProjectTypeDto {
+    private String projectTypeID;
+    private String projectTypeName;
+}
