b693367d65a5c087809ad0dfd5f04583cf234f6e
Nguyen Duc Quy <ducquyyy@gmail.com>
2024-02-23 13:48:31 +0700

    Thêm API tính tổng doanh thu của User trong AccountController

--- src/main/java/com/FTimeshare/UsageManagement/repositories/BookingRepository.java
+++ src/main/java/com/FTimeshare/UsageManagement/repositories/BookingRepository.java
@@ -1,15 +1,23 @@
 package com.FTimeshare.UsageManagement.repositories;
 
 import com.FTimeshare.UsageManagement.entities.BookingEntity;
+import com.FTimeshare.UsageManagement.entities.ProductEntity;
 import com.FTimeshare.UsageManagement.entities.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookingRepository extends JpaRepository<BookingEntity, String> {
 
-    @Query(value = "SELECT SUM(b.bookingPrice) FROM BookingEntity b where b.productID= :productID")
-    Float sumPriceByProductID(@Param("productID") int productID);
+    //bi loi but i don't know why
+//    @Query("SELECT SUM(b.bookingPrice) FROM BookingEntity b where b.productID.productID= :productID")
+//    double sumPriceByProductID(@Param("productID") int productID);
+
+    @Query("SELECT b FROM BookingEntity b where b.productID.productID= :productID")
+    List<BookingEntity> findByProductID(@Param("productID") int productID);
+
 }
