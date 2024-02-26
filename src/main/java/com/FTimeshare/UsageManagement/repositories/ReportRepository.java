494b740873e0d5ab2af4f83609549ceba6bcd88f
TuanDatCoder <tuandatdq03@gmail.com>
2024-02-26 20:02:56 +0700

    UPDATE DATABASE, VIEW ROLE

--- src/main/java/com/FTimeshare/UsageManagement/repositories/ReportRepository.java
+++ src/main/java/com/FTimeshare/UsageManagement/repositories/ReportRepository.java
@@ -1,5 +1,7 @@
 package com.FTimeshare.UsageManagement.repositories;
 
+import com.FTimeshare.UsageManagement.entities.PictureEntity;
+import com.FTimeshare.UsageManagement.entities.ProductEntity;
 import com.FTimeshare.UsageManagement.entities.ReportEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,4 +11,7 @@ import java.util.List;
 public interface ReportRepository extends JpaRepository<ReportEntity, Integer> {
     @Query("SELECT DISTINCT e.reportStatus FROM ReportEntity e")
     List<String> findAllStatus();
+    List<ReportEntity> findByProductID_ProductID(int productID);
+
+
 }
