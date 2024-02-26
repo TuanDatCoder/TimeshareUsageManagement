630ffd48f59390220ad9eb336be730b0a9123926
thaovy305 <ngothaovy305@gmail.com>
2024-02-26 11:53:06 +0700

    update picture

--- src/main/java/com/FTimeshare/UsageManagement/repositories/PictureRepository.java
+++ src/main/java/com/FTimeshare/UsageManagement/repositories/PictureRepository.java
@@ -1,10 +1,14 @@
 package com.FTimeshare.UsageManagement.repositories;
 
+import com.FTimeshare.UsageManagement.entities.BookingEntity;
 import com.FTimeshare.UsageManagement.entities.NewsEntity;
 import com.FTimeshare.UsageManagement.entities.PictureEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PictureRepository extends JpaRepository<PictureEntity, Integer> {
+    List<PictureEntity> findByProductID_ProductID(int productID);
 
 
 }
