1278367c73b77ae90561408e2e855d5c022c0124
TuanDatCoder <tuandatdq03@gmail.com>
2024-02-28 02:01:51 +0700

    Insert Image for Pictures Product

--- src/main/java/com/FTimeshare/UsageManagement/repositories/PictureRepository.java
+++ src/main/java/com/FTimeshare/UsageManagement/repositories/PictureRepository.java
@@ -6,9 +6,10 @@ import com.FTimeshare.UsageManagement.entities.PictureEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PictureRepository extends JpaRepository<PictureEntity, Integer> {
     List<PictureEntity> findByProductID_ProductID(int productID);
-
+    Optional<PictureEntity> findByImgName(String fileName);
 
 }
