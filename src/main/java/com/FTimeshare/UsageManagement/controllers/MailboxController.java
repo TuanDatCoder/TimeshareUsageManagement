35042226ed49025739b27e914db848774ef3c209
thaovy305 <ngothaovy305@gmail.com>
2024-03-02 17:31:55 +0700

    update mailbox

--- src/main/java/com/FTimeshare/UsageManagement/controllers/MailboxController.java
+++ src/main/java/com/FTimeshare/UsageManagement/controllers/MailboxController.java
@@ -1,27 +1,37 @@
 package com.FTimeshare.UsageManagement.controllers;
 
-import com.FTimeshare.UsageManagement.dtos.FeedbackDto;
+import com.FTimeshare.UsageManagement.dtos.MailboxDto;
+import com.FTimeshare.UsageManagement.services.MailboxService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/mailbox")
 public class MailboxController {
-    //@Autowired
+    @Autowired
+    private MailboxService mailboxService;
 
-   // @PostMapping("/customer/submitfeedback")
- //   public ResponseEntity<FeedbackDto> submitFeedback(@RequestBody FeedbackDto feedbackDto) {
-//        LocalDateTime now = LocalDateTime.now();
-//        feedbackDto.setFeedbackCreateDate(now);
-//
-//        FeedbackDto submittedFeedback = feedbackService.submitFeedback(feedbackDto);
-//        return new ResponseEntity<>(submittedFeedback, HttpStatus.CREATED);
-   // }
+    @PostMapping("/submitMail")
+    public ResponseEntity<?> submitMail(@RequestBody MailboxDto mailboxDto) {
+        LocalDateTime now = LocalDateTime.now();
+        mailboxDto.setMailboxSendDate(now);
+        MailboxDto submitMail = mailboxService.submitMail(mailboxDto);
+        return new ResponseEntity<>(submitMail, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/deleteMail/{mailboxId}")
+    public ResponseEntity<?> deleteMailBox(@PathVariable int mailboxId) {
+        MailboxDto deletedMail = mailboxService.deleteMailBox(mailboxId);
+
+        if (deletedMail != null) {
+            return ResponseEntity.ok(deletedMail);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
