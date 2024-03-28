df8e5c32085404d32632a1385a1b359ba2520d01
TuanDatCoder <tuandatdq03@gmail.com>
2024-03-28 23:41:30 +0700

    delete mailbox

--- src/main/java/com/FTimeshare/UsageManagement/services/PaymentService.java
+++ src/main/java/com/FTimeshare/UsageManagement/services/PaymentService.java
@@ -30,27 +30,17 @@ public class PaymentService {
             AccountEntity accountEntity = accountRepository.findById(accId)
                     .orElseThrow(() -> new IllegalArgumentException("Account not found with ID: " +accId));
 
-        String originalFilename = file.getOriginalFilename();
-        String filename = originalFilename;
-        int counter = 1;
-
-        while (paymentRepository.existsByImgName(filename)) {
-            filename = originalFilename.substring(0, originalFilename.lastIndexOf('.'))
-                    + "_" + counter
-                    + originalFilename.substring(originalFilename.lastIndexOf('.'));
-            counter++;
-        }
 
             PaymentEntity paymentEntity = paymentRepository.save(PaymentEntity.builder()
                     .accountName(accountName)
                     .banking(banking)
                     .accountNumber(accountNumber)
-                    .imgName(filename)
+                    .imgName(file.getName())
                     .imgData(ImageService.compressImage(file.getBytes()))
                     .accID(accountEntity)
                     .build());
 
-            return "File uploaded successfully: " + filename;
+            return "Create payment successfully";
         }
 
         public byte[] downloadImage(String fileName){
