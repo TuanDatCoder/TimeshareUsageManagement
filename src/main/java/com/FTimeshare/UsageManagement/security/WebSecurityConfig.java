2ed5dc37bea6adb31281ce8c073f4b7d5971dee9
Nguyen Duc Quy <ducquyyy@gmail.com>
2024-04-04 22:44:59 +0700

    Update

--- src/main/java/com/FTimeshare/UsageManagement/security/WebSecurityConfig.java
+++ src/main/java/com/FTimeshare/UsageManagement/security/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig{
                         exception -> exception.authenticationEntryPoint(jwtAuthEntryPoint))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/auth/login").permitAll()
+                        .requestMatchers("/auth/login").permitAll()
 //                        .requestMatchers("/api/users").permitAll()
 //                        .requestMatchers("/api/users/sendOTP/*").permitAll()
 //                        .requestMatchers("/api/users/verify/active/*").permitAll()
